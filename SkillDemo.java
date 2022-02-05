c730523bdd338890924b8e3f4e621f4b3ca8dd2c
therab6it <97642829+therab6it@users.noreply.github.com>
2022-02-04 16:44:07 -0800

    code + tester (fails)

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,16 @@
-// yes
+public class SkillDemo {
+    public static String songRecommender(String catalog, int index) {
+        
+        String[] minecraft = {"Living Mice", "Dreiton", "Sweden"};
+        String[] vSauce = {"Moon Men", "145(Poodles)", "It's Good to be D"};
+
+
+        if (catalog.equals("minecraft")) {
+            return minecraft[index];
+        } else {//if(catalog.equals("vSauce")) {
+            return vSauce[index];
+        // } else {
+        //     return "This catalog isn't available yet :(";
+        }
+    }
+}
--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -1,1 +1,10 @@
-//yess
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void recommender() {
+        assertEquals("This catalog isn't available yet :(", 
+            SkillDemo.songRecommender("CSGO", 1));
+    }
+}
